9972b7875f1bb417d07f552d8a3ec0173c290ce0
noonespecial009 <noonespecial009@gmail.com>
2020-05-01 09:50:27 -0700

    implement tests

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # fastapi-crud
+
+### Update image then run tests
+
+$ docker-compose up -d --build
+$ docker-compose exec web pytest .
--- src/app/main.py
+++ src/app/main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/ping")
 def pong():
     return {"ping": "pong!"}
+
+@app.get("/hello")
+def hello():
+    return {"hello": "hi!"}
